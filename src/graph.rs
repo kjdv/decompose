521fa3a8eed71d97490abf40adf35f8389130b80
Klaas de Vries <klaasjacobdevries@gmail.com>
2020-08-30 16:21:45 +0100

    node as trait

--- src/graph.rs
+++ src/graph.rs
@@ -7,29 +7,28 @@ use petgraph::Direction;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-#[derive(PartialEq, Eq, Debug, Clone)]
-pub struct Node {
-    name: String,
+pub trait Node {
+    fn from_config(p: &config::Program) -> Self;
 }
 
-pub struct Graph {
-    graph: petgraph::Graph<Node, ()>,
+pub struct Graph<T: Node> {
+    graph: petgraph::Graph<T, ()>,
 }
 
-impl Graph {
-    pub fn from_config(sys: config::System) -> Result<Graph> {
+impl<T: Node> Graph<T> {
+    pub fn from_config(sys: config::System) -> Result<Graph<T>> {
         let mut graph = petgraph::Graph::new();
 
         for prog in sys.program.iter() {
-            graph.add_node(Node {
-                name: prog.name.clone(),
-            });
+            graph.add_node(T::from_config(&prog));
         }
 
-        Graph::validate(&graph).map(|_| Graph { graph })
+        Graph::validate(&graph)?;
+
+        Ok(Graph { graph })
     }
 
-    fn validate(graph: &petgraph::Graph<Node, ()>) -> Result<()> {
+    fn validate(graph: &petgraph::Graph<T, ()>) -> Result<()> {
         assert!(graph.externals(Direction::Outgoing).any(|_| true));
         Ok(())
     }
@@ -39,7 +38,19 @@ impl Graph {
 mod tests {
     use super::*;
 
-    fn make(toml: &str) -> Graph {
+    struct TestNode {
+        name: String,
+    }
+
+    impl Node for TestNode {
+        fn from_config(p: &config::Program) -> Self {
+            TestNode {
+                name: p.name.clone(),
+            }
+        }
+    }
+
+    fn make(toml: &str) -> Graph<TestNode> {
         let cfg = config::System::from_toml(toml).unwrap();
         Graph::from_config(cfg).unwrap()
     }
@@ -55,16 +66,11 @@ mod tests {
         let graph = make(cfg);
         assert_eq!(1, graph.graph.node_count());
 
-        let entry_nodes: Vec<Node> = graph
+        let entry_nodes: Vec<String> = graph
             .graph
             .externals(Direction::Outgoing)
-            .map(|i| graph.graph[i].clone())
+            .map(|i| graph.graph[i].name.clone())
             .collect();
-        assert_eq!(
-            entry_nodes,
-            vec![Node {
-                name: "single".to_string()
-            }]
-        );
+        assert_eq!(entry_nodes, vec!["single".to_string()]);
     }
 }
